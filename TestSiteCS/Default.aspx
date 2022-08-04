bbe50480f5d4727f64381ab419ee9486598a722b
CasualGamer <alarich2@hotmail.com>
2022-08-04 18:54:16 +0200

    added testproject for C#

---
+++ TestSiteCS/Default.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="TestSiteCS.Default" %>
+<%@ Register Assembly="Alarich.WebPaint" Namespace="Alarich" TagPrefix="AHE" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <AHE:WebPaint ID="webPaint" runat="server" Width="500" Height="500" Visible="false" Language="EN" /><br />
+            <asp:Button ID="btnLoadImage" runat="server" OnClick="btnLoadImage_Click" Text="Load" />
+            <asp:Button ID="btnSaveImage" runat="server" OnClientClick="saveImageData();" OnClick="btnSaveImage_Click" Text="Save" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ TestSiteCS/Default.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TestSiteCS
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnLoadImage_Click(object sender, EventArgs e)
+        {
+            webPaint.DisplayImage("https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg");
+            webPaint.Visible = true;
+        }
+
+        protected void btnSaveImage_Click(object sender, EventArgs e)
+        {
+            string path = Server.MapPath("~/Images");
+            string fileName = DateTime.Now.ToString().Replace("/", "-").Replace(" ", "- ").Replace(":", "") + ".png";
+            string fullPath = path + "\\" + fileName;
+            webPaint.UploadImage(fullPath);
+            webPaint.Visible = false;
+        }
+    }
+}
